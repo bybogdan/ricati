8755f9619334844171fcb26fc7cda59d6921690f
bybogdan <varlamovbogdan@yandex.ru>
2023-02-02 10:06:10 +0400

    make

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ricati
+
+This project generates cover letter for job using AI.
+
+Based on [TwitterBio](https://github.com/Nutlope/twitterbio)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,152 @@
+import { AnimatePresence, motion } from "framer-motion";
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LoadingDots from "../components/LoadingDots";
+import ResizablePanel from "../components/ResizablePanel";
 
 const Home: NextPage = () => {
-  return <div>Ricati</div>;
+  const [loading, setLoading] = useState(false);
+  const [coverLetter, setCoverLetter] = useState("");
+  const [generatedCoverLetter, setGeneratedCoverLetter] = useState<string>("");
+
+  console.log("Streamed response: ", generatedCoverLetter);
+
+  const prompt = `Generate professional cover letter for job vacancy that will sure to attract the attention of the hiring manager on my resume and lead to an invitation to an interview and subsequent receipt the stunning offer from this company,. Make sure generated cover letter is at least 40 words and max 80 words, with clear start and end and base on this job vacancy description context: ${coverLetter}${
+    coverLetter.slice(-1) === "." ? "" : "."
+  }`;
+
+  const generateCoverLetter = async (e: any) => {
+    e.preventDefault();
+    setGeneratedCoverLetter("");
+    setLoading(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    console.log("Edge function returned.");
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedCoverLetter((prev) => prev + chunkValue);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>Ricati</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+        <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
+          Generate unique cover letter for a job vacancy in seconds
+        </h1>
+
+        <div className="max-w-xl w-full">
+          <div className="flex mt-10 items-center space-x-3">
+            <Image src="/1-black.png" width={30} height={30} alt="1 icon" />
+            <p className="text-left font-medium">Copy job description.</p>
+          </div>
+          <textarea
+            value={coverLetter}
+            onChange={(e) => setCoverLetter(e.target.value)}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              "e.g. Key Qualifications: Experience building apps and prototypes in a diversity of engine environments or 3D rendering toolkits. 4+ years writing software in an application programming language (Swift, C#, C++, etc) ..."
+            }
+          />
+
+          {!loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-2 hover:bg-black/80 w-full"
+              onClick={(e) => generateCoverLetter(e)}
+            >
+              Generate cover letter &rarr;
+            </button>
+          )}
+          {loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-2 hover:bg-black/80 w-full"
+              disabled
+            >
+              <LoadingDots color="white" style="large" />
+            </button>
+          )}
+        </div>
+
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{ duration: 1000 }}
+        />
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+
+        <ResizablePanel>
+          <AnimatePresence mode="wait">
+            <motion.div className="space-y-10 my-10">
+              {generatedCoverLetter && (
+                <>
+                  <div>
+                    <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                      Your generated bios
+                    </h2>
+                  </div>
+                  <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                    <div
+                      className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                      onClick={() => {
+                        navigator.clipboard.writeText(generatedCoverLetter);
+                        toast("Cover letter copied to clipboard", {
+                          icon: "✂️",
+                        });
+                      }}
+                      key={generatedCoverLetter}
+                    >
+                      <p>{generatedCoverLetter}</p>
+                    </div>
+                  </div>
+                </>
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </ResizablePanel>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
